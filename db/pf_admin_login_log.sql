33067d41a88a60962a4eea271d7f5edd64ea22de
alexQi <alex.qiubo@qq.com>
2016-10-11 18:10:17 +0800

    modified:   db/pf_admin_login_log.sql 	modified:   db/pf_admins.sql

--- db/pf_admin_login_log.sql
+++ db/pf_admin_login_log.sql
@@ -1,41 +1,103 @@
-INSERT INTO `pf_admin_login_log` VALUES (15, 64, '::1', '本机地址 ', '2016-9-29 13:47:04', '2016-9-29');
-INSERT INTO `pf_admin_login_log` VALUES (16, 64, '::1', '本机地址 ', '2016-9-29 13:57:26', '2016-9-29');
-INSERT INTO `pf_admin_login_log` VALUES (17, 64, '::1', '本机地址 ', '2016-9-29 15:06:30', '2016-9-29');
-INSERT INTO `pf_admin_login_log` VALUES (18, 64, '::1', '本机地址 ', '2016-9-29 15:13:08', '2016-9-29');
-INSERT INTO `pf_admin_login_log` VALUES (19, 64, '::1', '本机地址 ', '2016-9-29 17:15:10', '2016-9-29');
-INSERT INTO `pf_admin_login_log` VALUES (20, 64, '::1', '本机地址 ', '2016-9-29 17:27:38', '2016-9-29');
-INSERT INTO `pf_admin_login_log` VALUES (21, 64, '::1', '本机地址 ', '2016-9-30 09:11:50', '2016-9-30');
-INSERT INTO `pf_admin_login_log` VALUES (22, 59, '::1', '本机地址 ', '2016-9-30 09:44:13', '2016-9-30');
-INSERT INTO `pf_admin_login_log` VALUES (23, 64, '::1', '本机地址 ', '2016-9-30 11:21:10', '2016-9-30');
-INSERT INTO `pf_admin_login_log` VALUES (24, 64, '::1', '本机地址 ', '2016-9-30 11:35:15', '2016-9-30');
-INSERT INTO `pf_admin_login_log` VALUES (25, 64, '::1', '本机地址 ', '2016-9-30 12:00:08', '2016-9-30');
-INSERT INTO `pf_admin_login_log` VALUES (26, 64, '::1', '本机地址 ', '2016-9-30 15:07:54', '2016-9-30');
-INSERT INTO `pf_admin_login_log` VALUES (27, 64, '::1', '本机地址 ', '2016-9-30 15:11:41', '2016-9-30');
-INSERT INTO `pf_admin_login_log` VALUES (28, 64, '::1', '本机地址 ', '2016-9-30 15:13:28', '2016-9-30');
-INSERT INTO `pf_admin_login_log` VALUES (29, 64, '::1', '本机地址 ', '2016-9-30 15:14:26', '2016-9-30');
-INSERT INTO `pf_admin_login_log` VALUES (30, 64, '127.0.0.1', '本机地址 ', '2016-9-30 15:20:52', '2016-9-30');
-INSERT INTO `pf_admin_login_log` VALUES (31, 64, '::1', '本机地址 ', '2016-9-30 15:33:12', '2016-9-30');
-INSERT INTO `pf_admin_login_log` VALUES (32, 59, '::1', '本机地址 ', '2016-9-30 17:18:29', '2016-9-30');
-INSERT INTO `pf_admin_login_log` VALUES (33, 59, '::1', '本机地址 ', '2016-9-30 17:47:02', '2016-9-30');
-INSERT INTO `pf_admin_login_log` VALUES (34, 64, '127.0.0.1', '本机地址 ', '2016-9-30 18:00:30', '2016-9-30');
-INSERT INTO `pf_admin_login_log` VALUES (35, 64, '::1', '本机地址 ', '2016-10-8 09:40:11', '2016-10-8');
-INSERT INTO `pf_admin_login_log` VALUES (36, 64, '::1', '本机地址 ', '2016-10-8 09:42:55', '2016-10-8');
-INSERT INTO `pf_admin_login_log` VALUES (37, 64, '127.0.0.1', '本机地址 ', '2016-10-8 14:01:43', '2016-10-8');
-INSERT INTO `pf_admin_login_log` VALUES (38, 64, '::1', '本机地址 ', '2016-10-8 14:02:15', '2016-10-8');
-INSERT INTO `pf_admin_login_log` VALUES (39, 64, '::1', '本机地址 ', '2016-10-8 14:03:29', '2016-10-8');
-INSERT INTO `pf_admin_login_log` VALUES (40, 64, '127.0.0.1', '本机地址 ', '2016-10-8 14:03:43', '2016-10-8');
-INSERT INTO `pf_admin_login_log` VALUES (41, 64, '127.0.0.1', '本机地址 ', '2016-10-8 14:14:24', '2016-10-8');
-INSERT INTO `pf_admin_login_log` VALUES (42, 59, '127.0.0.1', '本机地址 ', '2016-10-8 14:36:32', '2016-10-8');
-INSERT INTO `pf_admin_login_log` VALUES (43, 64, '127.0.0.1', '本机地址 ', '2016-10-8 14:57:37', '2016-10-8');
-INSERT INTO `pf_admin_login_log` VALUES (44, 64, '127.0.0.1', '本机地址 ', '2016-10-8 15:04:07', '2016-10-8');
-INSERT INTO `pf_admin_login_log` VALUES (45, 64, '::1', '本机地址 ', '2016-10-8 15:49:25', '2016-10-8');
-INSERT INTO `pf_admin_login_log` VALUES (46, 64, '::1', '本机地址 ', '2016-10-8 15:49:30', '2016-10-8');
-INSERT INTO `pf_admin_login_log` VALUES (47, 64, '::1', '本机地址 ', '2016-10-8 15:49:33', '2016-10-8');
-INSERT INTO `pf_admin_login_log` VALUES (48, 64, '::1', '本机地址 ', '2016-10-8 15:50:47', '2016-10-8');
-INSERT INTO `pf_admin_login_log` VALUES (49, 64, '::1', '本机地址 ', '2016-10-8 15:51:02', '2016-10-8');
-INSERT INTO `pf_admin_login_log` VALUES (50, 64, '127.0.0.1', '本机地址 ', '2016-10-8 15:51:08', '2016-10-8');
-INSERT INTO `pf_admin_login_log` VALUES (51, 64, '::1', '本机地址 ', '2016-10-8 15:53:43', '2016-10-8');
-INSERT INTO `pf_admin_login_log` VALUES (52, 64, '::1', '本机地址 ', '2016-10-8 15:53:53', '2016-10-8');
-INSERT INTO `pf_admin_login_log` VALUES (53, 64, '::1', '本机地址 ', '2016-10-8 16:04:18', '2016-10-8');
-INSERT INTO `pf_admin_login_log` VALUES (54, 64, '::1', '本机地址 ', '2016-10-8 16:48:10', '2016-10-8');
-INSERT INTO `pf_admin_login_log` VALUES (55, 64, '::1', '本机地址 ', '2016-10-8 17:27:43', '2016-10-8');
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : local
+Source Server Version : 50617
+Source Host           : localhost:3306
+Source Database       : ffwap
+
+Target Server Type    : MYSQL
+Target Server Version : 50617
+File Encoding         : 65001
+
+Date: 2016-10-11 18:09:07
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for pf_admin_login_log
+-- ----------------------------
+DROP TABLE IF EXISTS `pf_admin_login_log`;
+CREATE TABLE `pf_admin_login_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `admin_id` int(11) NOT NULL,
+  `ip` varchar(20) CHARACTER SET utf8 NOT NULL,
+  `address` varchar(100) CHARACTER SET utf8 NOT NULL,
+  `time` datetime NOT NULL,
+  `date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=84 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of pf_admin_login_log
+-- ----------------------------
+INSERT INTO `pf_admin_login_log` VALUES ('15', '64', '::1', '本机地址 ', '2016-09-29 13:47:04', '2016-09-29');
+INSERT INTO `pf_admin_login_log` VALUES ('16', '64', '::1', '本机地址 ', '2016-09-29 13:57:26', '2016-09-29');
+INSERT INTO `pf_admin_login_log` VALUES ('17', '64', '::1', '本机地址 ', '2016-09-29 15:06:30', '2016-09-29');
+INSERT INTO `pf_admin_login_log` VALUES ('18', '64', '::1', '本机地址 ', '2016-09-29 15:13:08', '2016-09-29');
+INSERT INTO `pf_admin_login_log` VALUES ('19', '64', '::1', '本机地址 ', '2016-09-29 17:15:10', '2016-09-29');
+INSERT INTO `pf_admin_login_log` VALUES ('20', '64', '::1', '本机地址 ', '2016-09-29 17:27:38', '2016-09-29');
+INSERT INTO `pf_admin_login_log` VALUES ('21', '64', '::1', '本机地址 ', '2016-09-30 09:11:50', '2016-09-30');
+INSERT INTO `pf_admin_login_log` VALUES ('22', '59', '::1', '本机地址 ', '2016-09-30 09:44:13', '2016-09-30');
+INSERT INTO `pf_admin_login_log` VALUES ('23', '64', '::1', '本机地址 ', '2016-09-30 11:21:10', '2016-09-30');
+INSERT INTO `pf_admin_login_log` VALUES ('24', '64', '::1', '本机地址 ', '2016-09-30 11:35:15', '2016-09-30');
+INSERT INTO `pf_admin_login_log` VALUES ('25', '64', '::1', '本机地址 ', '2016-09-30 12:00:08', '2016-09-30');
+INSERT INTO `pf_admin_login_log` VALUES ('26', '64', '::1', '本机地址 ', '2016-09-30 15:07:54', '2016-09-30');
+INSERT INTO `pf_admin_login_log` VALUES ('27', '64', '::1', '本机地址 ', '2016-09-30 15:11:41', '2016-09-30');
+INSERT INTO `pf_admin_login_log` VALUES ('28', '64', '::1', '本机地址 ', '2016-09-30 15:13:28', '2016-09-30');
+INSERT INTO `pf_admin_login_log` VALUES ('29', '64', '::1', '本机地址 ', '2016-09-30 15:14:26', '2016-09-30');
+INSERT INTO `pf_admin_login_log` VALUES ('30', '64', '127.0.0.1', '本机地址 ', '2016-09-30 15:20:52', '2016-09-30');
+INSERT INTO `pf_admin_login_log` VALUES ('31', '64', '::1', '本机地址 ', '2016-09-30 15:33:12', '2016-09-30');
+INSERT INTO `pf_admin_login_log` VALUES ('32', '59', '::1', '本机地址 ', '2016-09-30 17:18:29', '2016-09-30');
+INSERT INTO `pf_admin_login_log` VALUES ('33', '59', '::1', '本机地址 ', '2016-09-30 17:47:02', '2016-09-30');
+INSERT INTO `pf_admin_login_log` VALUES ('34', '64', '127.0.0.1', '本机地址 ', '2016-09-30 18:00:30', '2016-09-30');
+INSERT INTO `pf_admin_login_log` VALUES ('35', '64', '::1', '本机地址 ', '2016-10-08 09:40:11', '2016-10-08');
+INSERT INTO `pf_admin_login_log` VALUES ('36', '64', '::1', '本机地址 ', '2016-10-08 09:42:55', '2016-10-08');
+INSERT INTO `pf_admin_login_log` VALUES ('37', '64', '127.0.0.1', '本机地址 ', '2016-10-08 14:01:43', '2016-10-08');
+INSERT INTO `pf_admin_login_log` VALUES ('38', '64', '::1', '本机地址 ', '2016-10-08 14:02:15', '2016-10-08');
+INSERT INTO `pf_admin_login_log` VALUES ('39', '64', '::1', '本机地址 ', '2016-10-08 14:03:29', '2016-10-08');
+INSERT INTO `pf_admin_login_log` VALUES ('40', '64', '127.0.0.1', '本机地址 ', '2016-10-08 14:03:43', '2016-10-08');
+INSERT INTO `pf_admin_login_log` VALUES ('41', '64', '127.0.0.1', '本机地址 ', '2016-10-08 14:14:24', '2016-10-08');
+INSERT INTO `pf_admin_login_log` VALUES ('42', '59', '127.0.0.1', '本机地址 ', '2016-10-08 14:36:32', '2016-10-08');
+INSERT INTO `pf_admin_login_log` VALUES ('43', '64', '127.0.0.1', '本机地址 ', '2016-10-08 14:57:37', '2016-10-08');
+INSERT INTO `pf_admin_login_log` VALUES ('44', '64', '127.0.0.1', '本机地址 ', '2016-10-08 15:04:07', '2016-10-08');
+INSERT INTO `pf_admin_login_log` VALUES ('45', '64', '::1', '本机地址 ', '2016-10-08 15:49:25', '2016-10-08');
+INSERT INTO `pf_admin_login_log` VALUES ('46', '64', '::1', '本机地址 ', '2016-10-08 15:49:30', '2016-10-08');
+INSERT INTO `pf_admin_login_log` VALUES ('47', '64', '::1', '本机地址 ', '2016-10-08 15:49:33', '2016-10-08');
+INSERT INTO `pf_admin_login_log` VALUES ('48', '64', '::1', '本机地址 ', '2016-10-08 15:50:47', '2016-10-08');
+INSERT INTO `pf_admin_login_log` VALUES ('49', '64', '::1', '本机地址 ', '2016-10-08 15:51:02', '2016-10-08');
+INSERT INTO `pf_admin_login_log` VALUES ('50', '64', '127.0.0.1', '本机地址 ', '2016-10-08 15:51:08', '2016-10-08');
+INSERT INTO `pf_admin_login_log` VALUES ('51', '64', '::1', '本机地址 ', '2016-10-08 15:53:43', '2016-10-08');
+INSERT INTO `pf_admin_login_log` VALUES ('52', '64', '::1', '本机地址 ', '2016-10-08 15:53:53', '2016-10-08');
+INSERT INTO `pf_admin_login_log` VALUES ('53', '64', '::1', '本机地址 ', '2016-10-08 16:04:18', '2016-10-08');
+INSERT INTO `pf_admin_login_log` VALUES ('54', '64', '::1', '本机地址 ', '2016-10-08 16:48:10', '2016-10-08');
+INSERT INTO `pf_admin_login_log` VALUES ('55', '64', '::1', '本机地址 ', '2016-10-08 17:27:43', '2016-10-08');
+INSERT INTO `pf_admin_login_log` VALUES ('56', '64', '::1', '本机地址 ', '2016-10-08 17:34:36', '2016-10-08');
+INSERT INTO `pf_admin_login_log` VALUES ('57', '64', '::1', '本机地址 ', '2016-10-09 09:05:49', '2016-10-09');
+INSERT INTO `pf_admin_login_log` VALUES ('58', '64', '::1', '本机地址 ', '2016-10-09 09:05:55', '2016-10-09');
+INSERT INTO `pf_admin_login_log` VALUES ('59', '64', '::1', '本机地址 ', '2016-10-09 09:06:52', '2016-10-09');
+INSERT INTO `pf_admin_login_log` VALUES ('60', '64', '::1', '本机地址 ', '2016-10-09 09:11:51', '2016-10-09');
+INSERT INTO `pf_admin_login_log` VALUES ('61', '59', '::1', '本机地址 ', '2016-10-09 09:16:55', '2016-10-09');
+INSERT INTO `pf_admin_login_log` VALUES ('62', '59', '::1', '本机地址 ', '2016-10-09 09:17:41', '2016-10-09');
+INSERT INTO `pf_admin_login_log` VALUES ('63', '59', '::1', '本机地址 ', '2016-10-09 09:20:34', '2016-10-09');
+INSERT INTO `pf_admin_login_log` VALUES ('64', '64', '::1', '本机地址 ', '2016-10-09 09:20:43', '2016-10-09');
+INSERT INTO `pf_admin_login_log` VALUES ('65', '64', '::1', '本机地址 ', '2016-10-09 09:24:55', '2016-10-09');
+INSERT INTO `pf_admin_login_log` VALUES ('66', '64', '::1', '本机地址 ', '2016-10-09 09:24:58', '2016-10-09');
+INSERT INTO `pf_admin_login_log` VALUES ('67', '64', '::1', '本机地址 ', '2016-10-09 09:25:00', '2016-10-09');
+INSERT INTO `pf_admin_login_log` VALUES ('68', '64', '::1', '本机地址 ', '2016-10-09 09:25:03', '2016-10-09');
+INSERT INTO `pf_admin_login_log` VALUES ('69', '64', '::1', '本机地址 ', '2016-10-09 11:10:55', '2016-10-09');
+INSERT INTO `pf_admin_login_log` VALUES ('70', '64', '::1', '本机地址 ', '2016-10-09 11:14:08', '2016-10-09');
+INSERT INTO `pf_admin_login_log` VALUES ('71', '64', '::1', '本机地址 ', '2016-10-09 11:56:04', '2016-10-09');
+INSERT INTO `pf_admin_login_log` VALUES ('72', '64', '::1', '本机地址 ', '2016-10-09 12:01:22', '2016-10-09');
+INSERT INTO `pf_admin_login_log` VALUES ('73', '64', '::1', '本机地址 ', '2016-10-10 09:43:42', '2016-10-10');
+INSERT INTO `pf_admin_login_log` VALUES ('74', '64', '::1', '本机地址 ', '2016-10-10 11:01:59', '2016-10-10');
+INSERT INTO `pf_admin_login_log` VALUES ('75', '64', '::1', '本机地址 ', '2016-10-10 15:26:46', '2016-10-10');
+INSERT INTO `pf_admin_login_log` VALUES ('76', '64', '127.0.0.1', '本机地址 ', '2016-10-10 15:56:57', '2016-10-10');
+INSERT INTO `pf_admin_login_log` VALUES ('77', '64', '::1', '本机地址 ', '2016-10-10 16:58:51', '2016-10-10');
+INSERT INTO `pf_admin_login_log` VALUES ('78', '64', '::1', '本机地址 ', '2016-10-11 09:06:43', '2016-10-11');
+INSERT INTO `pf_admin_login_log` VALUES ('79', '64', '::1', '本机地址 ', '2016-10-11 09:18:16', '2016-10-11');
+INSERT INTO `pf_admin_login_log` VALUES ('80', '64', '::1', '本机地址 ', '2016-10-11 09:21:34', '2016-10-11');
+INSERT INTO `pf_admin_login_log` VALUES ('81', '64', '::1', '本机地址 ', '2016-10-11 11:45:51', '2016-10-11');
+INSERT INTO `pf_admin_login_log` VALUES ('82', '64', '::1', '本机地址 ', '2016-10-11 14:43:41', '2016-10-11');
+INSERT INTO `pf_admin_login_log` VALUES ('83', '64', '::1', '本机地址 ', '2016-10-11 15:18:46', '2016-10-11');
--- db/pf_admins.sql
+++ db/pf_admins.sql
@@ -1,3 +1,50 @@
-INSERT INTO `pf_admins` VALUES (58, 'libaojie', '778669', '李保杰', '保杰', '08ab59358efbb47868a93bd0d617ae5c', 0, '2016-7-1 10:00:14', 0, 1, 2, 3, '319035193', 2147483647, 2147483647, '01-0|02-0|03-0|04-0|200-0,6,7,8,9,10,11,12,13,14,15|201-0,1,2,4,5,6|223-0|400-0|401-0|402-0|403-0|501-0|701-0|702-0|703-0|704-0,1,2,3,4|705-0,1,2,3,4,5,6|706-0,1,2,3,4', '1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31|32|33|34|35|36');
-INSERT INTO `pf_admins` VALUES (59, 'zhouhao', '778669', '周浩', '浩浩', '08ab59358efbb47868a93bd0d617ae5c', 5, '2016-10-8 14:36:32', 0, 1, 3, 3, '2147483647', 2147483647, 2147483647, '01-0|02-0|03-0|04-0|300-0,1,2,4|301-0,1,2,3,4,5,6,7,8,9,10,11|302-0|303-0|400-0|401-0|402-0|403-0|500-0|600-0,1,2,3,4,5,6|601-0,1', '1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31|32|33|34|35|36');
-INSERT INTO `pf_admins` VALUES (64, 'alex', '778669', '仇波', 'alex', '08ab59358efbb47868a93bd0d617ae5c', 37, '2016-10-8 17:27:43', 0, 1, 2, 3, '514438556', 2147483647, 2147483647, '01-0,11,12|100-0,101,102', '1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31|32|33|34|35|36');
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : local
+Source Server Version : 50617
+Source Host           : localhost:3306
+Source Database       : ffwap
+
+Target Server Type    : MYSQL
+Target Server Version : 50617
+File Encoding         : 65001
+
+Date: 2016-10-11 18:09:15
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for pf_admins
+-- ----------------------------
+DROP TABLE IF EXISTS `pf_admins`;
+CREATE TABLE `pf_admins` (
+  `admin_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_account` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `token` varchar(11) CHARACTER SET utf8 NOT NULL DEFAULT '514438556',
+  `true_name` varchar(120) CHARACTER SET utf8 NOT NULL,
+  `nick_name` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `password` varchar(40) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  `login_count` int(11) NOT NULL DEFAULT '0',
+  `last_time` datetime NOT NULL,
+  `is_closed` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `group_role` tinyint(2) NOT NULL DEFAULT '0' COMMENT '子公司',
+  `user_role` tinyint(1) NOT NULL DEFAULT '1' COMMENT '部门 (市场 研发 人事)',
+  `role_type` tinyint(2) NOT NULL DEFAULT '3' COMMENT '角色  （总经理，经理，员工）',
+  `qq` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `tel` int(25) NOT NULL DEFAULT '0' COMMENT 'telphone',
+  `mobile` int(25) NOT NULL,
+  `user_touch` text CHARACTER SET utf8 NOT NULL,
+  `area_limits` text CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`admin_id`),
+  UNIQUE KEY `admin_name` (`user_account`),
+  KEY `service` (`user_role`)
+) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=gbk COMMENT='管理员表';
+
+-- ----------------------------
+-- Records of pf_admins
+-- ----------------------------
+INSERT INTO `pf_admins` VALUES ('58', 'libaojie', '778669', '李保杰', '保杰', '08ab59358efbb47868a93bd0d617ae5c', '0', '2016-07-01 10:00:14', '0', '1', '2', '3', '319035193', '2147483647', '2147483647', '11-0|12-0|101-0,1,2,3,4,5|102-0', '1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31|32|33|34|35|36');
+INSERT INTO `pf_admins` VALUES ('59', 'zhouhao', '778669', '周浩', '浩浩', '08ab59358efbb47868a93bd0d617ae5c', '0', '2016-10-09 09:20:34', '0', '1', '3', '3', '2147483647', '2147483647', '2147483647', '11-0|12-0|101-0,1,2,3,4,5|102-0', '1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31|32|33|34|35|36');
+INSERT INTO `pf_admins` VALUES ('64', 'alex', '778669', '仇波', 'alex', '08ab59358efbb47868a93bd0d617ae5c', '5', '2016-10-11 15:18:46', '0', '1', '2', '3', '514438556', '2147483647', '2147483647', '11-0|12-0|101-0,1,2,3,4,5|102-0', '1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31|32|33|34|35|36');
